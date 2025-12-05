2091d014ce988829a59205d49acce2cd221e3c31
Kaneki <ernestito.dikoum@gmail.com>
2025-12-05 08:00:26 +0100

    Add ping route for health check

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,6 @@ Route::middleware('api')->group(function () {
     Route::post('/produits', [ProduitController::class, 'store']);
     Route::put('/produits/{id}', [ProduitController::class, 'update']);
     Route::delete('/produits/{id}', [ProduitController::class, 'destroy']);
+    Route::get('/ping', function () {
+    return response()->json(['status' => 'ok']);
 });
