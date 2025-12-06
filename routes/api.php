9877094211b14e36141fb537041daab44d6b19bd
Kaneki <ernestito.dikoum@gmail.com>
2025-12-06 01:37:33 +0100

    Fix ping route response formatting

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,19 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduitController;
 
 Route::apiResource('produits', ProduitController::class);
 
+// Groupe avec middleware API
 Route::middleware('api')->group(function () {
     Route::get('/produits', [ProduitController::class, 'index']);
     Route::post('/produits', [ProduitController::class, 'store']);
     Route::put('/produits/{id}', [ProduitController::class, 'update']);
     Route::delete('/produits/{id}', [ProduitController::class, 'destroy']);
+
+    // Route ping
     Route::get('/ping', function () {
-    return response()->json(['status' => 'ok']);
+        return response()->json(['status' => 'ok']);
+    });
 });
